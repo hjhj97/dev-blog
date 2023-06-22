c87925d8e8eb1bf990ee6d0738dfeb628ef005a3
JuHeonHa <hjhjaueon97@naver.com>
2023-06-22 16:47:34 +0900

    design(font) : noto-sans 로 폰트 변경

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
 // custom typefaces
 //import "@fontsource-variable/montserrat"
 //import "@fontsource/merriweather"
-import "@fontsource/nanum-gothic"
+//import "@fontsource/nanum-gothic"
+import "@fontsource/noto-sans-kr/500.css"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
