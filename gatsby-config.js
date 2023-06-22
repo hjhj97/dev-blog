efd2f1a7c280e2cd343ee6f796e2a04dd43086fb
JuHeonHa <hjhjaueon97@naver.com>
2023-06-22 19:39:03 +0900

    design : 파비콘 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/alphabat-upper-h.png`, // This path is relative to the root of the site.
+        icon: `src/images/neon-sign-h.png`, // This path is relative to the root of the site.
       },
     },
   ],
